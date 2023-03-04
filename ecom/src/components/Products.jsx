1940ef93975a91d447d726ad191fb95dafb5fd8f
Samuel priyam Mulpuri <87686877+Samuel-priyam@users.noreply.github.com>
2023-03-04 14:33:33 +0530

    fixing api issue

--- ecom/src/components/Products.jsx
+++ ecom/src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `https://ecom-first.onrender.com/api/products?category=${cat}`
+            : "https://ecom-first.onrender.com/api/products"
         );
         setProducts(res.data);
       } catch (err) {}
