e6641b6444e344d3f2a6a1829e47778a1a6b6da1
Samuel priyam Mulpuri <87686877+Samuel-priyam@users.noreply.github.com>
2023-03-04 14:40:16 +0530

    Update Navbar.jsx

--- ecom/src/components/Navbar.jsx
+++ ecom/src/components/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>ECOM.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
