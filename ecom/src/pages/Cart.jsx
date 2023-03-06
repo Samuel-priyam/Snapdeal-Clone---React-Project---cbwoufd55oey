0f10518bf79c6eb9da571de737337ec3febd8624
Samuel priyam Mulpuri <87686877+Samuel-priyam@users.noreply.github.com>
2023-03-06 15:35:17 +0530

    added bag quantity

--- ecom/src/pages/Cart.jsx
+++ ecom/src/pages/Cart.jsx
@@ -181,6 +181,9 @@ const Cart = () => {
     };
     stripeToken && makeRequest();
   }, [stripeToken, cart, history]);
+  
+    const quantity = useSelector(state=>state.cart.quantity)
+
   return (
     <Container>
       <Navbar />
@@ -190,7 +193,7 @@ const Cart = () => {
         <Top>
           <TopButton>CONTINUE SHOPPING</TopButton>
           <TopTexts>
-            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Shopping Bag({quantity})</TopText>
             <TopText>Your Wishlist (0)</TopText>
           </TopTexts>
           <TopButton type="filled">CHECKOUT NOW</TopButton>
